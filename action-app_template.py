e67f75bd7c806b22c54eeca6588dd12d9a129c8c
CoorFun <coorfang@gmail.com>
2019-10-02 16:07:21 +0200

    Updated to new callback and new block method

--- action-app_template.py
+++ action-app_template.py
@@ -8,17 +8,18 @@ from hermes_python.ontology.dialogue.intent import IntentMessage
 
 CONFIG_INI = "config.ini"
 
-# If this skill is supposed to run on the satellite,
+# if this skill is supposed to run on the satellite,
 # please get this mqtt connection info from <config.ini>
-# Hint: MQTT server is always running on the master device
+#
+# hint: MQTT server is always running on the master device
 MQTT_IP_ADDR: str = "localhost"
 MQTT_PORT: int = 1883
 MQTT_ADDR: str = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 
 class Template(object):
-    """Class used to wrap action code with mqtt connection
-       Please change the name refering to your application
+    """class used to wrap action code with mqtt connection
+       please change the name refering to your application
     """
 
     def __init__(self):
@@ -45,8 +46,8 @@ class Template(object):
 
         # if need to speak the execution result by tts
         hermes.publish_start_session_notification(
-                intent_message.site_id,
-                "Action 1", "")
+            intent_message.site_id,
+            "Action 1", "")
 
     @staticmethod
     def intent_2_callback(self,
@@ -63,26 +64,16 @@ class Template(object):
 
         # if need to speak the execution result by tts
         hermes.publish_start_session_notification(
-                intent_message.site_id,
-                "Action 2", "")
+            intent_message.site_id,
+            "Action 2", "")
 
-    @staticmethod
-    def master_intent_callback(self,
-                               hermes: Hermes,
-                               intent_message: IntentMessage,):
-
-        coming_intent = intent_message.intent.intent_name
-        if coming_intent == 'intent_1':
-            self.intent_1_callback(hermes, intent_message)
-        if coming_intent == 'intent_2':
-            self.intent_2_callback(hermes, intent_message)
-
-        # more callback and if condition goes here...
-
-    # --> Register callback function and start MQTT
+    # register callback function to its intent and start listen to MQTT bus
     def start_blocking(self):
         with Hermes(MQTT_ADDR) as h:
-            h.subscribe_intents(self.master_intent_callback).start()
+            h.subscribe_intent('intent_1', self.intent_1_callback)
+            .subscribe_intent('intent_2', self.intent_2_callback)
+            # more intents and callbacks go here...
+            .loop_forever()
 
 
 if __name__ == "__main__":
